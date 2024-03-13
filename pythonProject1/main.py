f62630547c02838177d27ec5318b23bd78f52ba7
Mateus Mota Nobrega-MNOBRE <mnobre@motorola.com>
2024-03-13 16:14:51 -0400

    #F558 - Usando o url_for para linkar as páginas do nosso site

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,5 +1,6 @@
 # render_template busca a pasta templates
-from flask import Flask, render_template
+#url for - mudar a url para o nome da função
+from flask import Flask, render_template, url_for
 
 # criação do site -- Começar por aqui
 app = Flask(__name__)
@@ -10,10 +11,10 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
-
-@app.route("/perfil")
-def perfil():
-    return render_template("perfil.html")
+# <usuario> - isso significa uma variavel, na qual podemos usar o python dentro do html/css
+@app.route("/perfil/<user>")
+def perfil(user):
+    return render_template("perfil.html", user=user)
 
 
 # simular servidor local, garantindo a sergurança para quandoe ele for implementado
